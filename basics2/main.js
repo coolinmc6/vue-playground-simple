bed61657ae6bac6c794a431f7746eae2a2b917d3
Colin McNamara <coolinmc6@gmail.com>
2020-12-23 13:57:37 -0500

    Simple Vuex counter to test concept

--- basics2/main.js
+++ basics2/main.js
@@ -1,8 +1,36 @@
 Vue.use(Vuex);
 
-var app = new Vue({
-    el: '#app',
-    data: {
-        name: 'World'
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
     }
+  }
+})
+
+var app = new Vue({
+  el: '#app',
+  store,
+  data: {
+    name: 'World'
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+    },
+    decrement() {
+      this.$store.commit('decrement')
+    }
+  },
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  }
 })
